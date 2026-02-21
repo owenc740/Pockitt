642d791091eb4d67752b7443c143640c6a39737a
Thomas Williams <145062446+twilltheslainshepard@users.noreply.github.com>
2026-02-21 21:45:04 +0000

    Modified RoomAssignHub, Room, Program, RoomService

--- Pockitt/Models/User.cs
+++ Pockitt/Models/User.cs
@@ -5,7 +5,6 @@ public class User
     public string ConnectionId { get; set; } = string.Empty;
     public string SessionToken { get; set; } = Guid.NewGuid().ToString();
     public string Username { get; set; } = string.Empty;
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
+    public string Geohash { get; set; } = string.Empty;
     public string RoomId { get; set; } = string.Empty;
 }
